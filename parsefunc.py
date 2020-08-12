2e8e0c8491696151418c5bb14eb0462eecf5a018
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-12 09:37:31 -0400

    Revert last commit
    actually no, the functions will do this themselves so that people copy pasting dont need to care

--- parsefunc.py
+++ parsefunc.py
@@ -7,14 +7,10 @@ from basicfunc import *
 
 class theBasics:
     def addition(n1, n2):
-        n1 = int(n1)
-        n2 = int(n2)
+        pass
     def subtraction(n1, n2):
-        n1 = int(n1)
-        n2 = int(n2)
+        pass
     def multiplication(n1, n2):
-        n1 = int(n1)
-        n2 = int(n2)
+        pass
     def division(n1, n2):
-        n1 = int(n1)
-        n2 = int(n2)
+        pass
