ec0ef307b37c7613782651905d522a002555066e
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-12 09:36:47 -0400

    Change to integers

--- parsefunc.py
+++ parsefunc.py
@@ -7,10 +7,14 @@ from basicfunc import *
 
 class theBasics:
     def addition(n1, n2):
-        pass
+        n1 = int(n1)
+        n2 = int(n2)
     def subtraction(n1, n2):
-        pass
+        n1 = int(n1)
+        n2 = int(n2)
     def multiplication(n1, n2):
-        pass
+        n1 = int(n1)
+        n2 = int(n2)
     def division(n1, n2):
-        pass
+        n1 = int(n1)
+        n2 = int(n2)
