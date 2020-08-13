fbed75570b747e99c908c88c70e51505aefea4e4
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-13 09:43:08 -0400

    fixes; add main method

--- parsefunc.py
+++ parsefunc.py
@@ -2,12 +2,18 @@
 Just a small...thing to Parse Functions from mathmod. Nothing to see here.
 """
 
-from func import *
-from basicfunc import *
+def main(theThing):
+    global _
+    _ = theThing
+
+from mathmod.func import *
+from mathmod.basicfunc import *
+from modules.cprint import *
 
 class theBasics:
     def addition(n1, n2):
-        pass
+        returnedNumber = add(n1, n2)
+        cprint.info(_("The response is...
     def subtraction(n1, n2):
         pass
     def multiplication(n1, n2):
