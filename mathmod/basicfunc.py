6d5786c7adcb9253e7dfb086de00eadd3f3a3d80
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-15 18:13:46 -0400

    remove exception checking
    The exception checking is already done in the parser, so why is it repeated,it's the program's responsibility for exception handling

--- mathmod/basicfunc.py
+++ mathmod/basicfunc.py
@@ -17,11 +17,8 @@ def getNum(): #ask for two numbers and then return to function
     return n1, n2
 
 def multi(): #multiplication
-    try:
-        n1 = float(n1)
-        n2 = float(n2)
-    except ValueError as ename:
-        raise ValueError("ERRID 3: %s" % ename)
+    n1 = float(n1)
+    n2 = float(n2)
     return (n1 * n2)
 def div(): #division
     """
@@ -38,18 +35,12 @@ def div(): #division
         cprint.err(_("There was an unknown issue dividing your Numbers..."))
         logging.error("User had an issue divvying up %s by %s (%s)" % (n1,n2,e))
 def sub(n1, n2): #subtraction
-    try:
-        n1 = float(n1) #I _could_ write a function (replacing `getNum()') that would take both numbers and return a tuple with the float'ed numbers, so it could be called like `n1, n2 = getNum(n1, n2)' but i'm too lazy ;D
-        n2 = float(n2)
-    except ValueError as ename:
-        raise ValueError("ERRID 3: %s" % ename)
+    n1 = float(n1)
+    n2 = float(n2)
     return (n1 - n2)
 def add(n1, n2): #addition
-    try:
-        n1 = float(n1)
-        n2 = float(n2)
-    except ValueError as ename:
-        raise ValueError("ERRID 3: %s" % ename)
+    n1 = float(n1)
+    n2 = float(n2)
     return (n1 + n2)
 
 
