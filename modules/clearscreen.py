063d251efc0c090620cc601ebbbbf3cde537b8a1
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-15 22:24:56 -0400

    Add comment

--- modules/clearscreen.py
+++ modules/clearscreen.py
@@ -4,8 +4,8 @@ def clearScreen():
     https://www.sololearn.com/Discuss/764024/how-to-clear-the-screen-in-python-terminal
     """
     import os
-    if os.name == "nt":
-        print (chr(12))
-    else:
+    if os.name == "nt": #Windows
+        print(chr(12))
+    else: #linux/macos
         esc = chr(27)
         print(esc + '[2J' + esc + '[0;0H')
