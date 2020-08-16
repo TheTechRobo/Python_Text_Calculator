e093289e57bc928f21c8e2591ea2dda98d57dada
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-15 22:17:34 -0400

    Update clearscreen.py

--- modules/clearscreen.py
+++ modules/clearscreen.py
@@ -1,5 +1,11 @@
 def clearScreen():
-    print(chr(27)+'[2j') #First attempt at clearing the screen with ANSI escape codes.
-    print('\033c') #Second attempt at clearing the screen with ANSI escape codes.
-    print('\x1bc') #Third attempt at clearing the screen with ANSI escape codes.
-    #(Credit for the above: https://raw.githubusercontent.com/TheTechRobo/python-text-calculator/master/FOR%20CLEARING%20THE%20SCREEN%20AND%20PRESS%20ANY%20KEY%20TO%20CONTINUE.md
+    """
+    Credit:
+    https://www.sololearn.com/Discuss/764024/how-to-clear-the-screen-in-python-terminal
+    """
+    import os
+    if os.name == "nt":
+        print (chr(12))
+    else:
+        esc = chr(27)
+        print(esc + '[2J' + esc + '[0;0H')
