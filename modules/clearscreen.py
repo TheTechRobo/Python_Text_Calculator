078e7eb0ba5969f0eb2eda342f839f8b1bf1bde3
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-15 22:28:04 -0400

    Add Old Functions to Clearscreen

--- modules/clearscreen.py
+++ modules/clearscreen.py
@@ -9,3 +9,14 @@ def clearScreen():
     else: #linux/macos
         esc = chr(27)
         print(esc + '[2J' + esc + '[0;0H')
+
+def clearScreenOld():
+    """This will work most of the time. Tries three times to clear the screen."""
+    print(chr(27)+'[2j') #first attempt at clearing the screen w/ ansi escape codes
+    print('\033c')#second attempt at clearing the screen w/ ansi escape codes
+    print('\x1bc')#third attempt at clearing the screen w/ ansi escape codes
+
+def clearScreenV1():
+    """Harder to understand but a two-liner. Doesn't work if the user doesn't have `clear' or `cls' installed."""
+    import os
+    os.system("cls" if os.name == "nt" else "clear")
