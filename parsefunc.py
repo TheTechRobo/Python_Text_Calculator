a7330139c0fc54dfc380b343abfa8b183f1e9dca
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-16 16:54:36 -0400

    this took ages
    Add div() thing, better code practice, users dont need to see a dump, better interactivity

--- parsefunc.py
+++ parsefunc.py
@@ -10,57 +10,83 @@ from mathmod.func import *
 from mathmod.basicfunc import *
 from modules.cprint import *
 import logging
+import copy
 
 class theBasics:
-    def addition(n1, n2):
+    def addition():
+        n1 = input(_("Please enter the first number: "))
+        n2 = input(_("Please enter the second number: "))
         try: 
             float(n1)
             float(n2)
         except Exception as ename:
-            cprint.err(_("ERRID 3: One or more of your numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
+            cprint.err(_("ERRID 3: One or more of your numbers was not a number."))
             raise ValueError(_("ERRID3: One or more of the two numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
         try:
             returnedNumber = add(n1, n2)
         except ValueError as ename:
-            logging.error("While parsing add(%(n1)s, %(n2)s), a ValueError was raised. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
+            logging.error("While parsing add(%(n1)s, %(n2)s), a ValueError was thrown. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
             cprint.info(_("An exception was raised!\nValueError\n"))
-            raise ValueError(ename)
+            raise
         cprint.info(_("The response is...%s"))
         logging.info("Parsed addition with %s as n1, %s as n2, answer = %s" % (n1, n2, returnedNumber))
-    def subtraction(n1, n2):
+    def subtraction():
+        n1 = input(_("Please enter the first number: "))
+        n2 = input(_("Please enter the second number: "))
         try:
             float(n1)
             float(n2)
         except Exception as ename:
-            cprint.err(_("ERRID 3: One or more of your numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
+            cprint.err(_("ERRID 3: One or more of your numbers was not a number." % (n1, n2)))
             raise ValueError(_("ERRID3: One or more of the numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
         try:
             returnedNumber = sub(n1, n2)
         except ValueError as ename:
-            logging.error("While parsing sub(%(n1)s, %(n2)s), a ValueError was raised. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
+            logging.error("While parsing sub(%(n1)s, %(n2)s), a ValueError was thrown. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
             cprint.info(_("An exception was raised!\nValueError\n"))
-            raise ValueError(ename)
+            raise
         cprint.info(_("The response is...%s"))
         logging.info("Parsed subtraction with %s as n1, %s as n2, answer as %s" % (n1, n2, returnedNumber))
-    def multiplication(n1, n2):
+    def multiplication():
+        n1 = input(_("Please enter the first number: "))
+        n2 = input(_("Please enter the second number: "))
         try:
             float(n1)
             float(n2)
         except Exception as ename:
-            cprint.err(_("ERRID 3: One or more of your numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
+            cprint.err(_("ERRID 3: One or more of your numbers was not a number."))
             raise ValueError(_("ERRID3: One or more of the numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
         try:
             returnedNumber = multi(n1, n2)
         except ValueError as ename:
-            logging.error("While parsing multi(%(n1)s, %(n2)s), a ValueError was raised. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
+            logging.error("While parsing multi(%(n1)s, %(n2)s), a ValueError was thrown. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
             cprint.info(_("An exception was raised!\nValueError\n"))
-            raise ValueError(ename)
+            raise
         cprint.info(_("The response is...%s"))
         logging.info("Parsed multiplication with %s as n1, %s as n2, answer as %s" % (n1, n2, returnedNumber))
-    def division(n1, n2):
-        """
-        Will do 'later'
-        """
+    def division():
+        n1 = input(_("Please enter the first number: "))
+        n2 = input(_("Please enter the second number: "))
+        try:
+            float(n1)
+            float(n2)
+        except Exception as ename:
+            cprint.err(_("ERRID 3: One or more of your numbers was not a number."))
+            raise ValueError(_("ERRID3: One or more of the numbers was not a number. (Dump: n1=%s, n2=%s)" % (n1, n2)))
+        try:
+            returnedNumber = multi(n1, n2)
+        except ZeroDivisionError:
+            logging.error("User decided to divide by zero.")
+            raise SyntaxError("yes, because dividing %s cookie(s) for %s friend(s) makes sense" % (n1, n2))
+        except ValueError as ename:
+            cprint.err(_("An exception was raised!\nValueError\n"))
+            logging.error("While parsing div(%(n1)s, %(n2)s), a ValueError was thrown. (%(error)s)" % {"n1": n1, "n2": n2, "error": ename})
+            raise
+        cprint.info(_("The response is...%s"))
+        logging.info("Parsed multiplication with %s as n1, %s as n2, answer as %s" % (n1, n2, returnedNumber))
+
+class misc:
+    """put the uc() area() etc here"""
 
 """
 these will have to be moved into a class later but for backwards compatibility it is temporarily kept
