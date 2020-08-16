3a6f9f9bd93b811bebaf6093b33a92a8e34c75f8
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-15 20:41:45 -0400

    fix????

--- modules/clearscreen.py
+++ modules/clearscreen.py
@@ -7,4 +7,4 @@ def clearScreen():
         print('\033c') #Second attempt at clearing the screen with ANSI escape codes.
         print('\x1bc') #Third attempt at clearing the screen with ANSI escape codes.
         #(Credit for the above: https://raw.githubusercontent.com/TheTechRobo/python-text-calculator/master/FOR%20CLEARING%20THE%20SCREEN%20AND%20PRESS%20ANY%20KEY%20TO%20CONTINUE.md
-    print("\n")
+    print("\n\n")
