3d9945617fc263a67f4815d19a99c43f99087df8
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-09-05 13:13:13 -0400

    remove unnecessary rubbish

--- mathmod/basicfunc.py
+++ mathmod/basicfunc.py
@@ -1,20 +1,7 @@
 # This file is for basic functions and small functions that would be in func.py but are too small to fit.
-import logging
-from modules.cprint import cprint
-from areaInteractive import *
-from volInteractive import *
-
-def main(Comandeer):
-    global _
-    _ = Comandeer
 
 if __name__ == "__main__":
     print("Please do not run any of these files directly. They don't do anything useful on their own.")
-def getNum(): #ask for two numbers and then return to function
-    n1 = int(input(_("Please enter the first number: ")))
-    n2 = int(input(_("Please enter the second number: ")))
-    logging.info("Palc got two numbers: %s and %s" % (n1, n2))
-    return n1, n2
 
 def multi(n1, n2): #multiplication
     n1 = float(n1)
