55eea2151158cb6574dc35b04c7776d2bebac53f
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-08 20:11:25 -0400

    Move stuff to parsefunc

--- mathmod/fibonacci.py
+++ mathmod/fibonacci.py
@@ -1,17 +1,5 @@
-"""
-TODO:
-Remove logging, it goes in ParseFunc.
-"""
-
 #FIBONACCI
 import time
-import logging
-def main(Comandeer):
-    """
-    TODO:
-    Remove (the `Press control c to stop' can be put in parsefunc)
-    """
-    globals()['_'] = Commandeer
 
 def CalculateFixedFibo(amount):
     """
@@ -41,31 +29,28 @@ def CalculateFixedFibo(amount):
 
 def CalculateLoopedFibo():
     """
-    this is Looped fibo which is indefinite.
+    This is looped fibonacci which is indefinite.
     """
-    print(_("Press Control-C to stop."))
     print("0, 1", end=", ")
-    try:
-        num0 = 0
-        num1 = 1
-        hi = 0
-        while True:
-            num = num0 + num1 #set variable num to the sum of num0 and num1.
-            if hi == 0:
-                num0 = num
-                hi = 1
-            else: #every other time this loops it will run this instead of the previous block
-                num1 = num # set num1 to num
-                hi = 0 #next time it wont do this block it'll do the previous one
-            print(num, end=", ", flush=True) #print the current number
-            time.sleep(0.5)
-    except KeyboardInterrupt: #if ctrl-c
-        print(_("Thanks for using Palc's FIBONACCI function!"))
-    except Exception as e: #if an error occurs
-        print(_("An error occured."))
-        logging.err("Exception %s in FIBONACCI" % e)
+    num0 = 0
+    num1 = 1
+    hi = 0
+    while True:
+        num = num0 + num1 #set variable num to the sum of num0 and num1.
+        if hi == 0:
+            num0 = num
+            hi = 1
+        else: #every other time this loops it will run this instead of the previous block
+            num1 = num # set num1 to num
+            hi = 0 #next time it wont do this block it'll do the previous one
+        print(num, end=", ", flush=True) #print the current number
+        time.sleep(0.5)
 
 if __name__ == "__main__":
-    def _(thestring):
-        return thestring
-    CalculateLoopedFibo()
+    print("Press Control-C to stop.")
+    try:
+        CalculateLoopedFibo()
+    except KeyboardInterrupt:
+        print("Thanks for using Mathmod's fibonacci function!")
+    except Exception as ename:
+        print("An error occured. (%s)" % ename)
