da4382fc8dad782fbe6917add93dd4915c8d518b
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-10-31 15:37:06 -0400

    Convert to float (dont rely on the program)

--- mathmod/area.py
+++ mathmod/area.py
@@ -1,6 +1,7 @@
 from math import sqrt, pi
 if __name__ == "__main__":
     print("Please do not run any of these files directly. They don't do anything useful on their own.")
+_f = float
 # Contains Area Calculating Functions
 
 # Triangles
@@ -13,6 +14,7 @@ def equtri(a: int) -> 'area':
     :param a: int
     :return: area
     '''
+    a = _f(a)
     area = (sqrt(3) / 4) * pow(a, 2)
     return area
 
@@ -26,6 +28,8 @@ def righttri(b: int, h: int) -> 'area':
     :param b: int
     :return: area
     '''
+    b = _f(b)
+    h = _f(h)
     area = 1 / 2 * b * h
     return area
 
@@ -41,6 +45,10 @@ def actri(a: int, b: int, c: int) -> 'area':
     :param c: int
     :return: area
     '''
+    
+    a = _f(a)
+    b = _f(b)
+    c = _f(c)
     s = (a + b + c) / 2
     area = sqrt(s * (s - a) * (s - b) * (s - c))
     return area
@@ -57,6 +65,9 @@ def obtri(a: int, b: int, c: int) -> 'area':
         :param c: int
         :return: area
         '''
+    a = _f(a)
+    b = _f(b)
+    c = _f(c)
     s = (a + b + c) / 2
     area = sqrt(s * (s - a) * (s - b) * (s - c))
     return area
@@ -72,6 +83,7 @@ def sq(a: int) -> 'area':
     :param a: int
     :return: area
     '''
+    a = _f(a)
     area = pow(a, 2)
     return area
 
@@ -85,6 +97,8 @@ def rectangle(l: int, b: int) -> 'area':
        :param b: int
        :return: area
        '''
+    l = _f(l)
+    b = _f(b)
     area = l * b
     return area
 
@@ -98,6 +112,8 @@ Takes 'b' As The Base And 'h' As The Height.
     :param h: int
     :return: area
     '''
+    a = _f(a)
+    b = _f(b)
     area = b * h
     return area
 
@@ -111,6 +127,8 @@ def rhombus(do: int, ds: int) -> 'area':
     :param ds: int
     :return:
     '''
+    do = _f(do)
+    ds = _f(ds)
     area = 1 / 2 * do * ds
     return area
 
@@ -125,6 +143,9 @@ def trapezium(a: int, b: int, h: int) -> 'area':
     :param h: int
     :return: area
     '''
+    a = _f(a)
+    b = _f(b)
+    h = _f(h)
     area = 1 / 2 * (a + b) * h
     return area
 
@@ -140,6 +161,7 @@ def circle(r: int) -> 'area':
     :param r:
     :return: area
     '''
+    r = _f(r)
     area = pi * (pow(r, 2))
     return area
 
@@ -152,6 +174,7 @@ def semicircle(r: int) -> 'area':
     :param r: int
     :return: area
     """
+    r = _f(r)
     area = 1 / 2 * (circle(r))
     return area
 
@@ -166,6 +189,8 @@ def cirsector(r: int, a: int) -> 'area':
     :param a: int
     :return: area
     """
+    r = _f(r)
+    a = _f(a)
     length = (a / 360) * 2 * pi * r
     area = 1 / 2 * length * r
     return area
@@ -181,6 +206,8 @@ def ring(ro: int, rs: int != 1) -> 'area':
     :param rs: int
     :return: area
     """
+    ro = _f(ro)
+    rs = _f(rs)
     area = pi * (pow(ro, 2) - pow(rs, 2))
     return area
 
@@ -194,5 +221,7 @@ def ellipse(a: int, b: int != 1) -> 'area':
     :param b: float
     :return: area
     """
+    a = _f(a)
+    b = _f(b)
     area = pi * a * b
     return area
