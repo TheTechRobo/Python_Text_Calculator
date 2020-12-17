fda70859cff00c35c126800fde42867d477a7d94
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-12-16 20:28:15 -0500

    MOVE TO PARSEFUNC

--- mathmod/func.py
+++ mathmod/func.py
@@ -7,25 +7,11 @@ def main(Comandeer):
 if __name__ == "__main__":
     print("Please do not run any of these files directly. They don't do anything useful on their own.")
 
-def calculateInterest():
-    while True: 
-        origin = int(input(_("What is the original number? ")))
-        rate = float(input(_("What is the rate of interest in percentage (without the percent sign)? ")))
-        print()
-        howMany = int(input(_('''How many units of time would you like to calculate? 
-Essentially, one unit of time could be one month, or one decade. It all depends on what you typed in the rate of interest question: it could be per year, per decade...we didn't ask.
-It was up to you to type the correct amount in the rate question.
-We have no idea what the rate represented: it could have been that rate per century for all we know.
-This calculator wasn't programmed with the ability to track time.
-So, with that out of the way, type the amount we should multiply the interest by (aka the amount of units of time).\nType it: ''')))
-        inRealNumbers = percentage(whole=origin, percent=rate)
-        number = origin + (inRealNumbers * howMany)
-        logging.info("INTERESTCALC: origin: %s rate: %s howMany: %s answer: %s" % (origin, rate, howMany, number))
-        cprint.info(_("The answer is: \n%s" % number))
-        doItAgain = input(_("Would you like to do it again (Y/n)? "))
-        doItAgain = doItAgain.lower()
-        if doItAgain == _("y"):
-            pass
-        else:
-            cprint.ok(_("Going back..."))
-            break
+"""def calculateInterest_RenameLater(origin, rate, units):
+    '''
+    units: if the rate is per month, and you want to calculate 3 months, you'd type 3 for this. If the rate is per year, and you want 2 years, you'd type 2 for this. And so on.
+    rate: How much money per unit of time. So if you want to do 5% per unit of time, you'd type 5. 15%? Type 15.
+    origin: Original number.
+    '''
+    inRealNumbers = percentage(whole=origin, percent=rate)
+"""
