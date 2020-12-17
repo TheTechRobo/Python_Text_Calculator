63222691411444555e71b4608998d187da6d461a
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-12-16 20:28:11 -0500

    MOVE LOG (requires refactoring for mathmod once its finished)

--- parsefunc.py
+++ parsefunc.py
@@ -349,6 +349,29 @@ def remember():
     memory.write(toRemember)
     logging.info("Saved number %s to memory slot %s" % (toRemember, slot))
 
+def calculateInterest():
+    while True: 
+        origin = int(input(_("What is the original number? ")))
+        rate = float(input(_("What is the rate of interest in percentage (without the percent sign)? ")))
+        print()
+        howMany = int(input(_('''How many units of time would you like to calculate? 
+Essentially, one unit of time could be one month, or one decade. It all depends on what you typed in the rate of interest question: it could be per year, per decade...we didn't ask.
+It was up to you to type the correct amount in the rate question.
+We have no idea what the rate represented: it could have been that rate per century for all we know.
+This calculator wasn't programmed with the ability to track time.
+So, with that out of the way, type the amount we should multiply the interest by (aka the amount of units of time).\nType it: ''')))
+        inRealNumbers = percentage(whole=origin, percent=rate)
+        number = origin + (inRealNumbers * howMany)
+        logging.info("INTERESTCALC: origin: %s rate: %s howMany: %s answer: %s" % (origin, rate, howMany, number))
+        cprint.info(_("The answer is: \n%s" % number))
+        doItAgain = input(_("Would you like to do it again (Y/n)? "))
+        doItAgain = doItAgain.lower()
+        if doItAgain[0] == _("y"):
+            pass
+        else:
+            cprint.ok(_("Going back..."))
+            return
+
 if __name__ == "__main__":
     print("Please don't run this file directly, it can only be used with Palc")
     try:
