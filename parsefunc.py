c0ea37d6bf811318bfd8f0b8f529a2e98bef1bd5
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-01-04 19:26:22 -0500

    Readability counts.

--- parsefunc.py
+++ parsefunc.py
@@ -497,6 +497,23 @@ class Area:
                 break
 
 class Volume:
+    choices = { #readability
+    "CUBE" = 1
+    "CUBOID" = 2
+    "CYLINDER" = 3
+    "HOLLOW CYLINDER" = 4
+    "CONE" = 5
+    "SPHERE" = 6
+    "EVIL" = 7
+    "HOLLOW SPHERE" = 8
+    "TRIANGULAR PRISM" = 9
+    "PENTAGONAL PRISM" = 10
+    "HEXAGONAL PRISM" = 11
+    "SQUARE-BASED PYRAMID" = 12
+    "TRIANGULAR PYRAMID" = 13
+    "PENTAGON-BASED PYRAMID" = 14
+    "HEXAGON-BASED PYRAMID" = 15
+    }
     def VolMain():
         cprint.info(_('''Options:
 1 - Cube
@@ -519,88 +536,88 @@ class Volume:
             except (ValueError, TypeError) as ename:
                 cprint.err("Please type an integer")
                 logging.error("User did a ValueError or TypeError while inputting choice in volinteractive (%s)" % ename)
-            if choice == 7:
+            if choice == Volume.choices["EVIL"]:
                 cprint.ok("Sorry, that was not an option. >:)")
                 logging.info(">:) choice 7")
-            elif choice == 1:
+            elif choice == Volume.choices["CUBE"]:
                 from mathmod.volume import vol_cube
                 a = float(input(_("What length is the side of the cube? ")))
                 volume = vol_cube(a)
                 logging.info("User ran Cuvolu(m) a=%s answer=%s" % (a, volume))
-            elif choice == 2:
+            elif choice == Volume.choices["CUBOID"]:
                 from mathmod.volume import vol_cuboid
                 b = float(input(_("What length is the breadth of the cuboid? ")))
                 h = float(input(_("What length is the height of the cuboid? ")))
                 l = float(input(_("What length is the cuboid? ")))
                 volume = vol_cuboid(b=b, h=h, l=l)
                 logging.info("User ran Cuboid Volume l=%s b=%s h=%s answer=%s" % (l, b, h, volume))
-            elif choice == 3:
+            elif choice == Volume.choices["CYLINDER"]:
                 from mathmod.volume import vol_cylinder
                 r = float(input(_("What is the radius of the cylinder? ")))
                 h = float(input(_("What is the height of the cylinder? ")))
                 volume = vol_cylinder(r=r, h=h)
                 logging.info("User ran Cylinder Volume r=%s h=%s answer=%s" % (r, h, volume))
-            elif choice == 4:
+            elif choice == Volume.choices["HOLLOW CYLINDER"]:
                 from mathmod.volume import vol_hollow_cylinder
                 ro = float(input(_("What is the radius of the hollow space? ")))
                 rs = float(input(_("What is the radius of the cylinder? ")))
                 h = float(input(_("What is the height of the cylinder? ")))
                 volume = vol_hollow_cylinder(ro=ro, rs=rs, h=h)
                 logging.info("User ran Hollowcylinder Volume ro=%s rs=%s h=%s answer=%s" % (ro, rs, h, volume))
-            elif choice == 5:
+            elif choice == Volume.choices["CONE"]:
                 from mathmod.volume import vol_cone
                 r = float(input(_("What is the radius of the cone? ")))
                 h = float(input(_("What is the height of the cone? ")))
                 volume = vol_cone(r=r, h=h)
                 logging.info("User ran Conevol r=%s h=%s answer=%s" % (r, h, volume))
-            elif choice == 6:
+            elif choice == Volume.choices["SPHERE"]:
                 from mathmod.volume import vol_sphere
                 r = float(input(_("What is the radius of the sphere? ")))
                 volume = vol_sphere(r)
                 logging.info("User ran sphere Volume r=%s answer=%s" % (r, volume))
-            elif choice == 8:
+            elif choice == Volume.choices["HOLLOW SPHERE"]:
                 from mathmod.volume import vol_hollow_sphere
                 ro = float(input(_("What is the radius of the sphere? ")))
                 rs = float(input(_("What is the radius of the hollow space? ")))
                 volume = vol_hollow_sphere(ro=ro, rs=rs)
                 logging.info("User ran Hollowsphere Volume ro=%s rs=%s answer=%s" % (ro, rs, volume))
-            elif choice == 9:
+            elif choice == Volume.choices["TRIANGULAR PRISM"]:
                 from mathmod.volume import vol_tri_prism
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the prism? ")))
                 volume = vol_tri_prism(a=a, h=h)
                 logging.info("User ran Triangle Prism Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 10:
+            elif choice == Volume.choices["PENTAGONAL PRISM"]:
                 from mathmod.volume import vol_penta_prism
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the prism? ")))
                 volume = vol_penta_prism(a=a, h=h)
                 logging.info("User ran PentaPrism Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 11:
+            elif choice == Volume.choices["HEXAGONAL PRISM"]:
                 from mathmod.volume import vol_hexa_prism
                 a = float(input(_("What is the length of the side of the hexagon? ")))
                 h = float(input(_("What is the height of the prism? ")))
                 volume = vol_hexa_prism(a=a, h=h)
                 logging.info("User ran Hexagon Prism Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 12:
+            elif choice == Volume.choices["SQUARE-BASED PYRAMID"]:
                 from mathmod.volume import vol_sqr_pyramid
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the pyramid? ")))
                 volume = vol_sqr_pyramid(a=a, h=h)
                 logging.info("User ran Square Pyramid Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 13:
+            elif choice == Volume.choices["TRIANGULAR PYRAMID"]:
                 from mathmod.volume import vol_tri_pyramid
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the pyramid? ")))
                 volume = vol_tri_pyramid(a=a, h=h)
                 logging.info("User ran Triangle Pyramid Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 14:
+            elif choice == Volume.choices["PENTAGON-BASED PYRAMID"]:
                 from mathmod.volume import vol_penta_pyramid
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the pyramid? ")))
                 volume = vol_penta_pyramid(a=a, h=h)
                 logging.info("User ran Pentapyramid Volume a=%s h=%s answer=%s" % (a, h, volume))
-            elif choice == 15:
+            elif choice == Volume.choices["HEXAGON-BASED PYRAMID"]:
                 from mathmod.volume import vol_hexa_pyramid
                 a = float(input(_("What is the length of the side of the base? ")))
                 h = float(input(_("What is the height of the pyramid? ")))
