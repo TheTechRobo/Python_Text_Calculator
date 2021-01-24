05c63e41e2b4f1a3eaac39041601c5a0d4941940
Ittussarom Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2021-01-24 16:49:07 -0500

    Improve version check.

--- setup_mathmod.py
+++ setup_mathmod.py
@@ -5,6 +5,37 @@ from setuptools import setup
 with open("README_MATHMOD.md", "r") as file:
     long_desc = file.read()
 
+import sys
+
+# **Python version check**
+# Source: https://github.com/ipython/ipython/blob/6a3e2db0c299dc05e636653c4a43d0aa756fb1c8/setup.py#L23-L58
+# This check is also made in mathmod/__init__, don't forget to update both when
+# changing Python version requirements.
+if sys.version_info < (3, 4):
+    pip_message = 'This may be due to an out of date pip. Make sure you have pip >= 9.0.1.'
+    try:
+        import pip
+        pip_version = tuple([int(x) for x in pip.__version__.split('.')[:3]])
+        if pip_version < (9, 0, 1) :
+            pip_message = 'Your pip version is out of date, please install pip >= 9.0.1, which will avoid installing this version of Mathmod. '\
+            'pip {} detected.'.format(pip.__version__)
+        else:
+            # pip is new enough - it must be something else
+            pip_message = ''
+    except Exception:
+        pass
+
+
+    error = """
+Mathmod 0.11+ supports Python 3.4 and above.
+When using earlier versions of Python, please install Mathmod 0.10 which may get updated occasionally for severe bug fixes.
+Python {py} detected.
+{pip}
+""".format(py=sys.version_info, pip=pip_message )
+
+    print(error, file=sys.stderr)
+    sys.exit(1)
+
 setup(
     name='mathmod',
     version='0.10.2',
@@ -29,6 +60,4 @@ setup(
     },
 
     long_description_content_type='text/markdown',
-
-    python_requires='>=3',
 )
