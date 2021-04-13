681674d0db4f2847eda97292bfa2ed1353876091
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-13 12:58:12 -0400

    Resorting to core words
    Dont worry, we'll do it better this time. What we're going to do _this_
    time is not ask for confirmation - just say what we assumed. This will
    also use less code.

--- parsefunc.py
+++ parsefunc.py
@@ -1,8 +1,12 @@
 import sys
+def showUserWhatIThink(msg):
+    cprint.ok(_("BTW, I parsed your choice as:\n%s") % msg)
+    logging.debug("Parsed user choice as %s" % msg)
 def parseCalc(calc):
+    logging.info("User entered `%s' % calc)
     calc = calc.replace('\u200b', '').replace('\\', '').replace('\'', '').strip().lower() #gets rid of the common stuff, without having to use core words and whatnot
-    if "/" in calc or calc == _("division"):
+    if "/" in calc or _("div") in calc:
         pass
-    elif calc == _("exit") or calc == _("quit") or calc == _("bye") or calc == _("leave"):
+    elif _("exit") in calc or _("quit") in calc or _("bye") in calc or _("leave") in calc:
         sys.exit()
     pass
