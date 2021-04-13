887bea72d654221cf9b6a24b2519a55d5c0e09b9
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-13 13:03:03 -0400

    When people are stupid, obnoxious, and daft...
    AND I FEEL LIKE I'VE HAD. QUITE ENOOOOOOOUUUUUGHHHHHHHHHHHHH!

--- parsefunc.py
+++ parsefunc.py
@@ -3,7 +3,7 @@ def showUserWhatIThink(msg):
     cprint.ok(_("BTW, I parsed your choice as:\n%s") % msg)
     logging.debug("Parsed user choice as %s" % msg)
 def parseCalc(calc):
-    logging.info("User entered `%s' % calc)
+    logging.info("User entered `%s'" % calc)
     calc = calc.replace('\u200b', '').replace('\\', '').replace('\'', '').strip().lower() #gets rid of the common stuff, without having to use core words and whatnot
     if "/" in calc or _("div") in calc:
         pass
