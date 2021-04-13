b5f20005c697bb6854419ffa8a2eb6e128df567c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-13 12:47:36 -0400

    add exit function
    to completely fix the builds

--- parsefunc.py
+++ parsefunc.py
@@ -1,2 +1,8 @@
+import sys
 def parseCalc(calc):
+    calc = calc.replace('\u200b', '').replace('\\', '').replace('\'', '').strip().lower() #gets rid of the common stuff, without having to use core words and whatnot
+    if "/" in calc or calc == _("division"):
+        pass
+    elif calc == _("exit") or calc == _("quit") or calc == _("bye") or calc == _("leave"):
+        sys.exit()
     pass
