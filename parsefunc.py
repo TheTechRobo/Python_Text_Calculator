e66d24df95abd11f5e22e41faa8ac5702a030fb2
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-14 23:06:33 -0400

    Add a help function

--- parsefunc.py
+++ parsefunc.py
@@ -9,5 +9,18 @@ def parseCalc(calc):
     if "/" in calc or _("div") in calc:
         pass
     elif _("exit") in calc or _("quit") in calc or _("bye") in calc or _("leave") in calc:
+        showUserWhatIThink("exit")
         sys.exit()
+    elif _("help") in calc:
+        cprint.info(helpText)
+    else:
+        cprint.err("UNKNOWN COMMAND - %s" % helpText.split('\n')[0]) #https://stackoverflow.com/a/11833277/9654083
+        cprint.ok("I'll help!")
+        cprint.info(helpText.split('\n')[2])
+        cprint.ok(helpText.split('\n')[3])
     pass
+helpText = """Using Palc but don't know what to do??
+I'll help!
+There are a bunch of commands you can use. These are: 
+\033[1mPlease enjoy Palc!\033[0m \033[94mFeedback or bug reports? Go to \033[4mgithub.com/thetechrobo/python-text-calculator/issues\033[0m\033[94m!\033[0m
+"""#https://stackoverflow.com/a/17303428/9654083
