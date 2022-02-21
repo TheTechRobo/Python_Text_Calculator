59d2ee2669bd1dd62430e5eab9df96e9e3cb601d
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-02-21 13:03:40 -0500

    tests for fibonacci and area

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import sys, mathmod, mathmod.area, mathmod.volume, mathmod.temperature
+import sys, mathmod, mathmod.area, mathmod.volume, mathmod.temperature, mathmod.fibonacci
 v = mathmod.volume
 t = mathmod.temperature
 from cprint import cprint
@@ -66,9 +66,20 @@ def mathmod_tests():
     tests = Testing()
     #tests.add_test("fail_always", True, fail)
     tests.add_test("mathmod::area::square", 25.0, test_area_square)
+    tests.add_test("mathmod::area::triangle", 2.5, lambda : mathmod.area.area_triangle(1, 5))
+    tests.add_test("mathmod::area::rectangle", 32.0, lambda:mathmod.area.area_rectangle( 8, 4))
+    tests.add_test("mathmod::area::parallelogram", 64.0, lambda:mathmod.area.area_parallelogram(8,8))
+    tests.add_test("mathmod::area::trapezium", 2.0, lambda:mathmod.area.area_trapezium(2,1,1))
+    tests.add_test("mathmod::area::circle", 12.566370614359172, lambda:mathmod.area.area_circle(2))
+    tests.add_test("mathmod::area::semicircle", 6.283185307179586, lambda:mathmod.area.area_semicircle(2))
+    tests.add_test("mathmod::area::ellipse", 91043.35510103221, lambda:mathmod.area.area_ellipse(69, 420))
+    tests.add_test("mathmod::area::sector", 6085800.0, lambda:mathmod.area.area_sector(69, 420))
+    tests.add_test("mathmod::area::rhombus", 28980.0, lambda :mathmod.area.area_rhombus(69, 420))
+    tests.add_test("mathmod::area::ring", 539219.8214694984, lambda:mathmod.area.area_ring(69,420))
     tests.add_test("mathmod::volume::cube", 64.0, test_volume_cube)
     tests.add_test("mathmod::temperature::cf", 37.4, test_temperature_cf)
     tests.add_test("mathmod::temperature::fcc", 3.0, test_temperature_fc)
+    tests.add_test("mathmod::fibonacci", [0, 1, 1, 2, 3], lambda : mathmod.fibonacci.calculate_fixed_fibonacci(5))
     tests.run()
 
 cprint.warn(":WARN: TEST SUITE UNFINISHED !")
